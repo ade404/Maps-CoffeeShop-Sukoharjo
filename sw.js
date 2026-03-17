2b6a4033954136dbd27209f0b01165706dcae3a7
putra ade <32617297+ade404@users.noreply.github.com>
2026-03-17 13:33:34 +0700

    sw-pwa

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "coffeedex-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./coffeeshop.json",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+];
+
+// Install Service Worker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    }),
+  );
+});
+
+// Fetching Assets
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    }),
+  );
+});
